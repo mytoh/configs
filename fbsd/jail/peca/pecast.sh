2f08598f91c392c345dc5659ecb269a75debc86f
mytoh <mytoh@gmail.com>
2015-05-12 21:09:36 +0900

    update jail settings

--- fbsd/jail/peca/pecast.sh
+++ fbsd/jail/peca/pecast.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-killall mono-sgen; exec mono ~/huone/ohjelmat/peercaststation/PeerCastStation.exe &
-#kill $(cat ~/pecast.lock) && rm ~/pecast.lock; exec mono-service -l:/home/mytoh/pecast.lock -d:/home/mytoh/huone/ohjelmat/peercaststation -m:PeerCastStation ~/huone/ohjelmat/peercaststation/PeerCastStation.exe 
+killall mono-sgen
+mono ~/huone/ohjelmat/peercaststation/PeerCastStation.exe &
+#kill $(cat ~/pecast.lock) && rm ~/pecast.lock; exec mono-service -l:/home/mytoh/pecast.lock -d:/home/mytoh/huone/ohjelmat/peercaststation -m:PeerCastStation ~/huone/ohjelmat/peercaststation/PeerCastStation.exe
 #exec mono --llvm --server ~/huone/ohjelmat/peercaststation/PeerCastStation.exe
