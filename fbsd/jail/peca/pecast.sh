c917ebb2694a4f76c1e85b6946cf26cd67e031bd
mytoh <mytoh@gmail.com>
2016-01-08 18:59:15 +0900

    * fbsd/jail/peca/pecast.sh: using deamon version

--- fbsd/jail/peca/pecast.sh
+++ fbsd/jail/peca/pecast.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
-killall mono-sgen
-mono ~/huone/ohjelmat/peercaststation/PeerCastStation.exe &
-#kill $(cat ~/pecast.lock) && rm ~/pecast.lock; exec mono-service -l:/home/mytoh/pecast.lock -d:/home/mytoh/huone/ohjelmat/peercaststation -m:PeerCastStation ~/huone/ohjelmat/peercaststation/PeerCastStation.exe
+
+APPDIR=~/huone/ohjelmat/peercaststation
+LOCKFILE=~/pecast.lock
+
+if test -e $APPDIR/PecaStationd.exe
+then
+    if test -e ${LOCKFILE}
+    then
+        printf "* Found Lock File\n"
+        printf "* Killing %s\n" $(cat ${LOCKFILE})
+        kill $(cat ${LOCKFILE})
+        rm ${LOCKFILE}
+    fi
+    sleep 1 # not sure this is needed
+    printf "* Running PecaStationd.exe\n"
+    mono-service -l:${LOCKFILE} ${APPDIR}/PecaStationd.exe
+    printf "* Started PecaStationd.exe\n"
+else
+    killall mono-sgen; exec mono ${APPDIR}/PeerCastStation.exe &
+fi
+
 #exec mono --llvm --server ~/huone/ohjelmat/peercaststation/PeerCastStation.exe
